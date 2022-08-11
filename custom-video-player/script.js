4a313b3e6ff3143c3c7dd093646bded181e6fd2c
Lauren <wilkins.laurenr@gmail.com>
2022-08-11 09:41:50 -0400

    add movie seat booking and custom video player

---
+++ custom-video-player/script.js
@@ -0,0 +1,64 @@
+const video = document.getElementById('video');
+const play = document.getElementById('play');
+const stopBtn = document.getElementById('stop');
+const progress = document.getElementById('progress');
+const time = document.getElementById('time');
+
+// Play & pause video
+function toggleVideoStatus() {
+    if(video.paused) {
+        video.play();
+    } else {
+        video.pause();
+    }
+}
+
+function updatePlayIcon() {
+    if(video.paused) {
+        play.innerHTML = '<i class="fa fa-play fa-2x"></i>';
+    } else {
+        play.innerHTML = '<i class="fa fa-pause fa-2x"></i>';
+    }
+
+}
+
+// update progress & timestamp
+function updateProgress() {
+    progress.value = (video.currentTime / video.duration) * 100;
+
+    // Get minutes
+    let mins = Math.floor(video.currentTime / 60);
+    if(mins < 10) {
+        mins = '0' + String(mins);
+    }
+    // Get seconds
+    let secs = Math.floor(video.currentTime % 60);
+    if(secs < 10) {
+        secs = '0' + String(secs);
+    }
+
+    timestamp.innerHTML = `${mins}:${seconds}`;
+}
+
+// Set video time to progress
+function setVideoProgress() {
+    video.currentTime = (+progress.value * video.duration) / 100;
+}
+
+// Stop video
+function stopVideo() {
+    video.currentTime = 0;
+    video.pause();
+}
+
+// Event listeners
+video.addEventListener('click', toggleVideoStatus);
+video.addEventListener('pause', toggleVideoStatus);
+video.addEventListener('play', toggleVideoStatus);
+video.addEventListener('timeupdate', toggleVideoStatus);
+
+play.addEventListener('click', toggleVideoStatus);
+
+stopBtn.addEventListener('click', stopVideo);
+
+progress.addEventListener('change', setVideoProgress);
